5b09385419bab14bd765fb3f08adcdbd53a17db7
Philipp Kursawe <phil.kursawe@gmail.com>
2020-04-19 01:23:23 +0200

    feat: Add counters to split tweets

--- index.js
+++ index.js
@@ -1,27 +1,49 @@
+const COUNTERS_REGEX = /{(count|total)}/gi;
+
+function replaceCounters(text, counters) {
+  return text.replace(COUNTERS_REGEX, function(_, match) {
+    return counters[match] || match
+  })
+}
+
 module.exports = (text, limit = 280, joiner = '...') => {
   if (text.length <= limit) {
     return [text];
   }
+  const hasCounters = COUNTERS_REGEX.test(joiner);
+
+  const joinerLength = hasCounters
+    ? replaceCounters(joiner, { count: 99, total: 99 }).length
+    : joiner.length;
 
   let newText = text;
   const result = [];
 
   do {
-    const cutoff = newText.lastIndexOf(' ', limit - (joiner.length * (result.length ? 2 : 1)));
+    const cutoff = newText.lastIndexOf(' ', limit - (joinerLength * ((!hasCounters && result.length) ? 2 : 1)));
 
     if (cutoff === -1) {
       throw new Error(`The text cannot be split with a limit: ${limit}, joiner: ${joiner}`);
     }
 
     let chunk = newText.slice(0, cutoff) + joiner;
-    if (result.length) {
+    if (!hasCounters && result.length) {
       chunk = joiner + chunk;
     }
     result.push(chunk);
     newText = newText.slice(cutoff + 1);
-  } while (newText.length > limit - joiner.length);
+  } while (newText.length > limit - joinerLength);
 
-  result.push(joiner + newText);
-
-  return result;
+  if (!hasCounters) {
+    result.push(joiner + newText);
+    return result;
+  } else {
+    result.push(newText + joiner);
+    return result.map(function(line, index) {
+      return replaceCounters(line, {
+        total: result.length,
+        count: index + 1
+      });
+    });
+  }
 };
